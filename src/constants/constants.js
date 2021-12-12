88711088e44b68d876d0bc93f7adecd57f42d13c
Sanskar2001 <sanskaratrey22@gmail.com>
2021-12-13 02:07:45 +0530

    added project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -22,53 +22,60 @@ export const projects = [
     id: 3,
   },
   {
+    title: 'Video Call Web App',
+    description: "It is a video calling application where users can call other user.Users can instantly join a video call with a code and chat in real-time.",
+      image: '/images/videoChatPreview.png',
+    visit: 'https://github.com/Sanskar2001/Video-Call-App',
+    id: 4,
+  },
+  {
     title: 'Android Message App',
     description: "A social messaging application which can be used to connect with friends in realtime.Uses Firebase.",
       image: '/images/AndroidMessage.png',
     visit: 'https://github.com/Sanskar2001/Message_APK',
-    id: 4,
+    id: 5,
   },
   {
     title: 'Android To Do App',
     description:"A task management application which helps in staying organised and can make a list of tasks to do.Uses Room Persistence Library.",
     image: '/images/ToDoPreview.png',
     visit: 'https://github.com/Sanskar2001/todolist',
-    id: 5,
+    id: 6,
   },
   {
     title: 'Android Social Media App',
     description: "A social media image sharing android based application. Uses RESTful API",
       image: '/images/AndroidSocialMedia.png',
     visit: 'https://github.com/Sanskar2001/SocialMedia',
-    id: 6,
+    id: 7,
   },
   {
     title: 'Android News App',
     description: "A android based news application.It is a news aggregator that organizes and highlights what’s happening in the world.Uses RESTful API",
     image: '/images/AndroidNewsApp1.png',
     visit: 'https://github.com/Sanskar2001/NewsApp',
-    id: 7,
+    id: 8,
   },
   {
     title: 'Android Flappy Bird Game',
     description: "A android based game application.The classic flappy bird game built using libGDX",
     image: '/images/AndroidFlappyBird.png',
     visit: 'https://github.com/Sanskar2001/Android-Flappy-Bird',
-    id: 8,
+    id: 9,
   },
   {
     title: 'iOS Chat App',
     description: "An iOS based chat application.Built using Firebase",
     image: '/images/iosFlashChat.png',
     visit: 'https://github.com/Sanskar2001/Flash_Chat_iOS',
-    id: 9,
+    id: 10,
   },
   {
     title: 'iOS Weather App',
     description: "An iOS based weather application.Uses RESTful API",
     image: '/images/iosWeather.png',
     visit: 'https://github.com/Sanskar2001/Climate_iOS',
-    id: 10,
+    id: 11,
   },
   
   {
@@ -76,16 +83,8 @@ export const projects = [
     description: "An iOS based ToDo application.Uses NSPersistentContainer for data persistance",
     image: '/images/iosToDo.png',
     visit: 'https://github.com/Sanskar2001/ToDo_iOS',
-    id: 11,
+    id: 12,
   }
   
   
-];
-
-export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
 ];
